4b561303868feb22d98346f31b199b31242b1865
heren <heren1heren@gmail.com>
2023-08-17 13:53:51 -0600

    first day

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # CalculatorProject
-
-Step 1:  Create variables for Numbers after calculate.
+*Step 1:  Create variables for Numbers after calculate.
  Add a pretty  webpage
 1.1:
  let operator, addednum, substractedNum, multipliedNum, dividedNum
@@ -12,19 +11,22 @@ Step 1:  Create variables for Numbers after calculate.
 1.3: Creating a website
  *18 buttons
  * 1 main flexbox > *4 flexbox
-
-1.3.1
-1.3.2
-1.3.3
-
-diving into webpage appearance;
+/*somehow done*/
 
 
 
 
-Step 2: ....
 
 
 
 
-Step 3:...
+Step 2:
+Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+
+
+
+Step 3: Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
+
+    You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
+    This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
--- calculatorProject.js
+++ calculatorProject.js
@@ -1,65 +1,57 @@
+const calculatorContainer = document.getElementById('calculator');
+const displayArea = document.getElementById('display')
+let currentText = '';
+
+calculatorContainer.addEventListener('click', (e) => {
+   const clickedButton = e.target 
+   // Run display function and store number, operators in a, b, operator
+   const buttonText = clickedButton.textContent;
+   if (clickedButton.classList.contains('num')) {
+        currentText += buttonText;
+        displayArea.textContent = currentText;
+   }
+});
+
+function display(input) {
+    
+}
+
 let result = 0, a = 0, b = 0;
 let operator = '';
 
 function add(firstNum, b) {
     return result = a + b;
-
 }
 
 function subtract(a, b) {
-return result = a - b;
-
+    return result = a - b;
 }
 
 function multiply(a, b) {
-return result = a * b;
-
+    return result = a * b;
 }
 
 function divide(a, b) {
- return result = a / b;
-
+    return result = a / b;
 }
-/* i will implement the logic to trigger operatorChooser later
- what happen when :
-  press 2 numbs without operator =>
-  press 2 operators continuously =>
-  press alternatively 1 numb and 1 operator =>
-  press ....
-  press  'clear' button? => ???
-  press  'backspace' button? => ???
-*/
- 
-function operatorChooser(operator, a, b) // execute  when press '=' key on the calculator;
-// or when press another operator , 
 
-{    // assign result to a again  
-     
-    switch(operator) {
-        case '+'  :   
-            add(a,b);
-        break;
-        
-        case '-' :   
+function operatorChooser(operator, a, b) {
+    // Assign result to a again
+    switch (operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
             subtract(a, b);
-        break;
-
-        case '*' :   
-            multiply(a,b);
-        break;
-
-        case '/' :   
-            divide(a,b);
-        break;
-        default: return  result = null;
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+        default:
+            result = null;
     }
     return result;
 }
-
-
-operatorChooser('-', 5, -5);
-console.log(a);
-console.log(result);
-a = operatorChooser('-', 5, -5);
-console.log(a == result);
-// so far so good move on to create the website:.... yyeahhh
