2506917ccd8153e7f761c0364ee1a1c794f982d5
heren <heren1heren@gmail.com>
2023-08-17 05:43:29 -0600

    First step

---
+++ calculatorProject.js
@@ -0,0 +1,29 @@
+let addedNum = 0, substractedNum = 0;
+let multipliedNum = 0, dividedNum = 0;
+
+
+function add(firstNum, secondNum) {
+    return addedNum = firstNum + secondNum;
+
+}
+
+function subtract(firstNum, secondNum) {
+return substractedNum = firstNum - secondNum;
+
+}
+
+function multiply(firstNum, secondNum) {
+return multipliedNum = firstNum * secondNum;
+
+}
+
+function divide(firstNum, secondNum) {
+ return dividedNum = firstNum / secondNum;
+
+}
+ 
+
+
+
+
+
