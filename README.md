364a48c76ecaf453261bb3e328d276d7342ec64d
heren <heren1heren@gmail.com>
2023-08-17 06:20:09 -0600

    after 1 hours

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # CalculatorProject
+
+Step 1:  Create variables for Numbers after calculate.
+ Add a pretty  webpage
+1.1:
+ let operator, addednum, substractedNum, multipliedNum, dividedNum
+ /*done*/
+1.2:
+ Create 4 functions for calculate and 1 function to decide how to perform these functions based on the 'operator' variable and reset the 'result' variable as the next 'a'.
+/*done*/
+
+1.3: 
+
+diving into webpage appearance;
+
+
+
+
+Step 2: ....
+
+
+
+
+Step 3:...
--- calculatorProject.js
+++ calculatorProject.js
@@ -1,29 +1,68 @@
-let addedNum = 0, substractedNum = 0;
-let multipliedNum = 0, dividedNum = 0;
+let result = 0;
+let operator = '';
 
-
-function add(firstNum, secondNum) {
-    return addedNum = firstNum + secondNum;
-
-}
-
-function subtract(firstNum, secondNum) {
-return substractedNum = firstNum - secondNum;
+function add(firstNum, b) {
+    return result = a + b;
 
 }
 
-function multiply(firstNum, secondNum) {
-return multipliedNum = firstNum * secondNum;
+function subtract(a, b) {
+return result = a - b;
 
 }
 
-function divide(firstNum, secondNum) {
- return dividedNum = firstNum / secondNum;
+function multiply(a, b) {
+return result = a * b;
 
 }
+
+function divide(a, b) {
+ return result = a / b;
+
+}
+/* i will implement the logic to trigger operatorChooser later
+ what happen when :
+  press 2 numbs without operator =>
+  press 2 operators continuously =>
+  press alternatively 1 numb and 1 operator =>
+  press ....
+  press  'clear' button? => ???
+  press  'backspace' button? => ???
+*/
  
+function operatorChooser(operator, a, b) // execute  when press '=' key on the calculator;
+// or when press another operator , 
 
+{   a = result; // assign result to a again  
+     
+    switch(operator) {
+        case '+'  :   
+            add(a,b);
+        break;
+        
+        case '-' :   
+            subtract(a, b);
+        break;
 
+        case '*' :   
+            multiply(a,b);
+        break;
 
+        case '/' :   
+            divide(a,b);
+        break;
+        default: return  result = null;
+        
+    }
+     
+    
+} 
+
+operator = '+';
+a = 3;
+b = 33;
+
+operatorChooser(operator,a ,b);
+console('result');
 
 
